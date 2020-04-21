4f367af31542af652649507fb2564a1ec97fe829
trylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2020-04-21 13:35:18 -0500

    Update render.js

--- src/render.js
+++ src/render.js
@@ -4,6 +4,7 @@ const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 const videoSelectBtn = document.getElementById('videoSelectBtn');
 videoSelectBtn.onclick = getVideoSources; //assigns event handler
+const { Menu } = remote; //allows building of native menus in frontend code
 
 const { dekstopCapturer } = require('electron');
 
@@ -12,6 +13,14 @@ async function getVideoSources() {
     const inputSources = await dekstopCapturer.getSources({
         types: ['window', 'screen']
     });
-    
+
+    const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return {
+                label: source.name,
+                clock: () => selectSource()
+            };
+        })
+    );
 
 }
