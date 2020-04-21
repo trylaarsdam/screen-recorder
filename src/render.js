3320d446f4b443fd4f7bfb8da1e6d4842ab74adf
trylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2020-04-21 13:44:55 -0500

    Update render.js

--- src/render.js
+++ src/render.js
@@ -4,9 +4,9 @@ const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 const videoSelectBtn = document.getElementById('videoSelectBtn');
 videoSelectBtn.onclick = getVideoSources; //assigns event handler
+const { desktopCapturer, remote } = require('electron');
 const { Menu } = remote; //allows building of native menus in frontend code
 
-const { desktopCapturer } = require('electron');
 
 //Get the available video sources
 async function getVideoSources() {
