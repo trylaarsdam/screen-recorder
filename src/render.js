3a41e3f632940c81a73970a7b4d9e071b294a95d
trylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2020-04-21 13:32:25 -0500

    Update render.js

--- src/render.js
+++ src/render.js
@@ -3,12 +3,13 @@ const videoElement = document.querySelector('video');
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources; //assigns event handler
 
 const { dekstopCapturer } = require('electron');
 
 //Get the available video sources
-async function getvideoSources() {
+async function getVideoSources() {
 
 
-    
+
 }
